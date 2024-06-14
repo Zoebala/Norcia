1a626a68d73c7a230d38e6b5cf707fde8dd6c387
Zoé Bala <zoebala288@gmail.com>
2024-06-14 11:04:34 +0100

    création des modèles et leurs migrations

---
+++ app/Models/Pointvente.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pointvente extends Model
+{
+    use HasFactory;
+}
