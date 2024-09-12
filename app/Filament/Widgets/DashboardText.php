a712570ba05fdb42696df37a20c8fe276816c381
Zoé Bala <zoebala288@gmail.com>
2024-09-12 20:55:43 +0100

    définition de l'année de travail au niveau du tableau de bord

---
+++ app/Filament/Widgets/DashboardText.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\Widget;
+
+class DashboardText extends Widget
+{
+    protected static bool $isLazy = false;
+    protected static string $view = 'filament.widgets.dashboard-text';
+}
